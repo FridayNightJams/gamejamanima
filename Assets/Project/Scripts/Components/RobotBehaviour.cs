41050363e162889c8045f6d6c551623b1eb3ef30
Pedro de Quadra <pedrodequadra@gmail.com>
2021-11-01 22:50:14 -0300

    Erro na rotação do personagem - verificar. Ajustado modelos de personagem, lixo e lixeira.

--- Assets/Project/Scripts/Components/RobotBehaviour.cs
+++ Assets/Project/Scripts/Components/RobotBehaviour.cs
@@ -7,6 +7,8 @@ public class RobotBehaviour : MonoBehaviour
     [SerializeField] public bool isTentaPegarLixo;
     [SerializeField] public bool isSoltaLixo;
     [SerializeField] public Transform lixoPrefab;
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject playerModel;
 
     public IEnumerator Start()
     {
@@ -14,25 +16,34 @@ public class RobotBehaviour : MonoBehaviour
         {
             foreach (Command c in CommandData.Instance().GetBuild())
             {
-                yield return new WaitForSeconds(1);
-                if (c.id.Contains("Mover")) Mover(c.position);
+                yield return new WaitForSeconds(5);
+                if (c.id.Contains("Mover")) Mover(c.position, c.direcao);
                 if (c.id.Equals("Pegar")) PegarLixo();
                 if (c.id.Equals("Soltar")) SoltarLixo();
+
             }
         }
     }
 
-    private void Mover(Vector3 pos)
+    private void Mover(Vector3 pos, Quaternion rot)
     {
         isTentaPegarLixo = false;
         transform.position += pos;
+        playerModel.transform.localRotation = new Quaternion(playerModel.transform.localRotation.x, rot.y, playerModel.transform.localRotation.z, rot.w);
         Debug.Log("Mover");
+        animator.SetBool("walking", true);
+    }
+
+    private void MoverPersonagem(Vector3 pos)
+    {
+
     }
 
     private void PegarLixo()
     {
         Debug.Log("PegarLixo");
         isTentaPegarLixo = true;
+        animator.SetBool("walking", false);
     }
 
     private void SoltarLixo()
@@ -40,6 +51,7 @@ public class RobotBehaviour : MonoBehaviour
         isTentaPegarLixo = false;
         Debug.Log("SoltarLixo");
         isSoltaLixo = true;
+        animator.SetBool("walking", false);
     }
 
     private void OnTriggerStay(Collider other)
